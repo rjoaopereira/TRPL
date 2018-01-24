d8f947e46fef628545a13b9bd8156d09e23a01a1
Ricardo Pereira <ricardo.pereira@blip.pt>
2018-01-24 10:35:13 +0000

    The Rust Programming Language Book

---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/ricardopereira/workspace/personal/rust/TRPL/guessing_game/target/debug/guessing_game: /Users/ricardopereira/workspace/personal/rust/TRPL/guessing_game/src/main.rs
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/ricardopereira/workspace/personal/rust/TRPL/hello_world/target/debug/hello_world: /Users/ricardopereira/workspace/personal/rust/TRPL/hello_world/main.rs
